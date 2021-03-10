62248201e219a13605411d8e1cf3ab089209e74e
Rubens Almeida <rubpalmeida@gmail.com>
2021-03-10 20:38:39 -0300

    Changing components to tsx

--- src/components/RepositoryItem.tsx
+++ src/components/RepositoryItem.tsx
@@ -1,8 +1,15 @@
-export function RepositoryItem(props) {
+interface RepositoryItemProps {
+  repository: {
+    name: string,
+    description: string,
+    html_url: string,
+  }
+}
+
+export function RepositoryItem(props: RepositoryItemProps) {
+
   return (
     <li>
-      <img src={props.repository.owner.avatar_url} alt="" />
-
       <strong>{props.repository.name}</strong>
       <p>{props.repository.description}</p>
 
